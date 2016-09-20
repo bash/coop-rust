41a2618dbeba595eba1fd8e0a2c71ec444506cd9
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-09-20 18:51:03 +0200

    Fix timestamp issue

--- src/coop/time.rs
+++ src/coop/time.rs
@@ -2,12 +2,15 @@ extern crate time;
 
 use time::Tm;
 
+// There's a  bug in the API that stores timestamps with a 2 hour offset
+const OFFSET: i64 = 60 * 60 * 2;
+
 pub fn midnight() -> i64 {
-    let mut now: Tm = time::now();
+    let mut now: Tm = time::now_utc();
 
     now.tm_hour = 0;
     now.tm_min = 0;
     now.tm_sec = 0;
 
-    return now.to_timespec().sec;
+    return now.to_timespec().sec - OFFSET;
 }
