252ff8c4e96ba6eefcf807db30de5eb56cb52ebf
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-09-27 11:43:43 +0200

    Add case-insensitive autocomplete

--- scripts/bash_completion.sh
+++ scripts/bash_completion.sh
@@ -2,10 +2,14 @@
 
 _COOP_CACHE_FILE="${HOME}/.coop-locations-cache"
 
-_coop_locations() {
+_coop_get_locations() {
   coop locations \
     | grep -e  "- *" \
     | sed -e 's/\-//g'
+}
+
+_coop_lowercase () {
+    echo ${1} | tr '[:upper:]' '[:lower:]'
 }
 
 _coop_cached_locations() {
@@ -23,8 +27,23 @@ _coop_cached_locations() {
     cat ${_COOP_CACHE_FILE}
   else
     touch ${_COOP_CACHE_FILE}
-    _coop_locations | tee ${_COOP_CACHE_FILE}
+    _coop_get_locations | tee ${_COOP_CACHE_FILE}
   fi
+}
+
+_coop_locations() {
+    local cur loc locations lower_loc
+
+    locations=$(_coop_cached_locations)
+    cur=$(_coop_lowercase ${COMP_WORDS[COMP_CWORD]})
+
+    for loc in ${locations}; do
+        lower_loc=$(_coop_lowercase "$loc")
+
+        if [[ "$lower_loc" == "$cur"* ]]; then
+            COMPREPLY+=("$loc")
+        fi
+    done
 }
 
 _coop () {
@@ -32,12 +51,13 @@ _coop () {
 
   COMPREPLY=()
   cur=${COMP_WORDS[COMP_CWORD]}
+  cur_lower=$(echo ${cur} | tr '[:upper:]' '[:lower:]')
   prev=${COMP_WORDS[COMP_CWORD-1]}
 
   opts="menus locations dish-stats"
 
   if [ "${prev}" = "menus" ]; then
-    COMPREPLY=( $(compgen -W "${prev} $(_coop_cached_locations)" -- ${cur}) )
+    _coop_locations
   else
     COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
   fi
