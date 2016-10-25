31b7c3f220fdc67f35ca80d72e2899bf4cfa9e4e
Ruben Schmidmeister <ruben.schmidmeister@icloud.com>
2016-10-25 15:20:32 +0200

    Remove api patch

--- src/coop/time.rs
+++ src/coop/time.rs
@@ -2,9 +2,6 @@ extern crate time;
 
 use time::Tm;
 
-// There's a  bug in the API that stores timestamps with a 2 hour offset
-const OFFSET: i64 = 60 * 60 * 2;
-
 pub fn midnight() -> i64 {
     let mut now: Tm = time::now_utc();
 
@@ -12,5 +9,5 @@ pub fn midnight() -> i64 {
     now.tm_min = 0;
     now.tm_sec = 0;
 
-    return now.to_timespec().sec - OFFSET;
+    return now.to_timespec().sec;
 }
